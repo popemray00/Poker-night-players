8554ba6bcca5d7da0990044e649cbbd61cc8f2ac
popemray00 <popemray@yahoo.com>
2019-01-22 13:34:47 -0800

    added delete button and update link to player.js

--- client/src/components/Player.js
+++ client/src/components/Player.js
@@ -9,6 +9,12 @@ const Player = props => {
                 <h3>{props.player.age}</h3>
                 <h3>Favorite Hand:{props.player.fav_hand}</h3>
                 <img className="player-img" src={props.player.img_url} alt={props.player.name}/><br></br>
+
+                <button onClick={ () => props.destroyPlayer(props.player.id)}>DELETE PLAYER</button>
+                <button> 
+                    <link to={`/players/${props.player.id}/edit`} onclick={ () => props.preFillInfo({
+                        name: props.player.name, age: props.player.age, img_url: props.player.img_url, fav_hand: props.player.fav_hand })} className="link">UPDATE PLAYER</link>
+                </button>
             </div>
         </div>
     )
