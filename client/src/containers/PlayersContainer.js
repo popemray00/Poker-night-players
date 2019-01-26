3ca9ac145491741f0a2ce6ffa23debf192a03100
popemray00 <popemray@yahoo.com>
2019-01-26 07:38:05 -0800

    Players container started

--- client/src/containers/PlayersContainer.js
+++ client/src/containers/PlayersContainer.js
@@ -1,10 +1,34 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
+import Player from '../components/Player';
+import { getPlayers, destroyPlayer } from '../actions/PlayerActions';
+import { preFillForm } from '../actions/PlayerFormAction'
 
 class PlayersContainer extends Component {
 
+    componentDidMount() {
+        this.props.getPlayers()
+    }
+
     render() {
-        return();
+        return(
+            <div className='players-container'>
+                <div className='App-header'>
+                <h1>Poker Players</h1>
+                
+                {this.props.players.map(player => <Player key={player.id} player={player} destroyPlayer={this.props.destroyPlayer} preFillForm={this.props.preFillForm}/>)}
+                <div>{this.props.children}</div>
+                </div>
+            </div>
+        );
     }
 }
 
-export default PlayersContainer;
+const mapStateToProps = (state) => {
+    return ({
+        players: state.players.players
+    })
+}
+
+export default connect(mapStateToProps, {getPlayers, destroyPlayer, preFillForm})(PlayersContainer);
